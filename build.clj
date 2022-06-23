867588df870ac047c138a21b9436eca4d22ac6ba
Wes Morgan <git@wesmorgan.me>
2022-06-23 12:41:23 -0600

    Get docker linux builds working

--- build.clj
+++ build.clj
@@ -31,16 +31,17 @@
           str/lower-case
           (str/index-of "win")))
 
-(def native-image-bin
-  (cond-> "native-image"
-          windows? (str ".cmd")))
+(defn bin-name
+  [base-name & [win-ext]]
+  (let [win-ext (or win-ext ".exe")]
+    (cond-> base-name
+            windows? (str win-ext))))
 
 (defn native-image-path
   [& [os arch]]
   (str/join File/separator ["target" version
                             (str (or os os-name) "-" (or arch os-arch))
-                            "opawssm"]))
-
+                            (bin-name "opawssm")]))
 
 (defn clean [_]
   (b/delete {:path "target"}))
@@ -66,7 +67,8 @@
     (println "Building binary in" nip)
     (io/make-parents nip)
     (let [ni-home (or (System/getenv "GRAALVM_HOME")
-                      (System/getenv "JAVA_HOME"))]
+                      (System/getenv "JAVA_HOME"))
+          native-image-bin (bin-name "native-image" ".cmd")]
       (b/process {:command-args
                   [(str/join File/separator [ni-home "bin" native-image-bin])
                    "-jar" jar-file
@@ -83,33 +85,18 @@
                  (str "--platform=linux/" arch)
                  (str "--build-arg=CLJ_VERSION=" docker-clojure-version)
                  (str "--build-arg=GRAAL_VERSION=" docker-graal-version)
-                 "--load" "-t" "opawssm-builder" "."]})
+                 "--load" "-t" "opawssm-builder:local" "."]})
     (b/process {:command-args
                 ["docker" "rm" "-f" "opawssm-builder"]})
     (b/process {:command-args
                 ["docker" "create" (str "--platform=linux/" arch)
-                 "--name" "opawssm-builder" "opawssm-builder"
-                 "ls" "-la"]})
-    (b/process {:command-args
-                ["docker" "run" (str "--platform=linux/" arch)
-                 "--rm" "opawssm-builder"
-                 "ls" "-la" "target"]})
-    (b/process {:command-args
-                ["docker" "run" (str "--platform=linux/" arch)
-                 "--rm" "opawssm-builder"
-                 "ls" "-la" "target/0.1.0"]})
-    (b/process {:command-args
-                ["docker" "run" (str "--platform=linux/" arch)
-                 "--rm" "opawssm-builder"
-                 "ls" "-la" "target/0.1.0/*/"]})
+                 "--name" "opawssm-builder" "opawssm-builder:local"]})
     (io/make-parents path)
-    (b/process {:command-args
-                ["docker" "start" "-a" "opawssm-builder"]})
     (b/process {:command-args
                 ["docker" "cp"
                  (str "opawssm-builder:/opawssm/" path)
-                 path]})))
-    ;(b/process {:command-args ["docker" "rm" "opawssm-builder"]})))
+                 path]})
+    (b/process {:command-args ["docker" "rm" "opawssm-builder"]})))
 
 (defn all [_]
   (native-image nil)
